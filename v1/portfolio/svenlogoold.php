f99c4818dbeb9363b05cae07797b4b3154d0d7f7
Sveinn Davidsson <s@keepitcomplicated.com>
2013-08-14 10:09:36 +0000

    Version 1

---
+++ v1/portfolio/svenlogoold.php
@@ -0,0 +1,22 @@
+<?php @include_once($_SERVER["DOCUMENT_ROOT"]."/shortstat/inc.stats.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Image</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="../styles/style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+body {
+	margin-left: 20px;
+}
+-->
+</style>
+</head>
+
+<body>
+	<p id="portfolio"><img src="images/sven.gif" width="500" height="127"></p>
+	<p>The very old, and probably the first logo I ever made. </p>
+</body>
+</html>
